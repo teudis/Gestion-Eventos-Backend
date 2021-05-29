ed21c763f3279f12eaacf1884a7e3c482387003c
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2021-05-29 18:58:00 -0400

    Actualizacion

--- backend/README.md
+++
@@ -1,8 +0,0 @@
-Sistema de Hidro Recursos Desarrollado
-
-- Symfony
-- Bootstrap
-- Backend(SonataAdminBundle)
-
-
-
--- backend/src/Web/CompanyBundle/WebCompanyBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Web\CompanyBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class WebCompanyBundle extends Bundle
-{
-}
